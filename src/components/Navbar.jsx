dd781c358505aa389a76c5ac2005eba93a089e04
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-09-04 07:53:47 -0400

    working on Navbar's UI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,13 +8,15 @@ import { RESIZE } from "../constants/keywords";
 const MyNavbar = () => {
   const [isExpanded, setIsExpanded] = useState(false);
 
-  const resizeHanler = (e) => {
+  const resizeHanler = () => {
     const winWidth = window.innerWidth;
 
     winWidth > 992 ? setIsExpanded(true) : setIsExpanded(false);
   };
 
   useEffect(() => {
+    resizeHanler();
+
     window.addEventListener(RESIZE, resizeHanler);
 
     return () => window.removeEventListener(RESIZE, resizeHanler);
@@ -22,19 +24,37 @@ const MyNavbar = () => {
 
   return (
     <Navbar
-      className={`${styles.Navbar} h-90`}
-      aria-controls="brand-logo"
+      className={`${styles.Navbar} d-flex flex-row`}
+      aria-controls="brand-name"
       aria-expanded={isExpanded}
       expand="lg"
     >
       <Container>
-        <Collapse in={isExpanded}>
-          <Navbar.Brand id="brand-logo">
-            <a href="brand">
-              <img src={Logo} alt="brand-logo" />
+        {isExpanded && (
+          <Navbar.Brand id="brand-name">
+            <a href="#brand">
+              <img
+                src={Logo}
+                alt="logo"
+                class="pull-left visible-md visible-lg"
+              />
             </a>
           </Navbar.Brand>
-        </Collapse>
+        )}
+        <h1>Daivd Chu's China Bistro</h1>
+      </Container>
+      <Container>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarToggleExternalContent"
+          aria-controls="navbarToggleExternalContent"
+          aria-expanded="true"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
       </Container>
     </Navbar>
   );
