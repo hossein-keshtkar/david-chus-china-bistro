47a93ee17a57e69a7f4383d672c5fb4674aa9268
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-09-04 16:10:06 -0400

    stuck

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Container, Navbar, Collapse } from "react-bootstrap";
 
 import logo from "../assets/images/restaurant-logo_medium.png";
+import logoLg from "../assets/images/restaurant-logo_large.png";
 import star from "../assets/images/star-k-logo.png";
 import styles from "../styles/Navbar.module.css";
 import { RESIZE } from "../constants/keywords";
@@ -25,62 +26,83 @@ const MyNavbar = () => {
 
   return (
     <Navbar
-      className={`${styles.Navbar}`}
+      className={`${styles.Navbar} navbar navbar-expand-lg`}
       aria-controls="brand-name"
       aria-expanded={isExpanded}
       expand="lg"
     >
-      <Container className="d-flex flex-row">
-        <div>
-          {isExpanded && (
-            <Navbar.Brand id="brand-name">
-              <a href="#brand">
-                <img
-                  src={logo}
-                  alt="logo"
-                  class="pull-left visible-md visible-lg"
-                />
-              </a>
-            </Navbar.Brand>
-          )}
-        </div>
-        <div>
-          <h1 className={`${styles.navbarHeader} navbar-header`}>
-            Daivd Chu's China Bistro
-          </h1>
-          <div className="d-flex align-items-center">
-            <span className="me-2">
-              <img src={star} alt="start-k-logo" />
-            </span>
-            <div className="pt-1">KOSHER CERTIFIED</div>
+      <Container>
+        <div className="d-flex flex-row">
+          <Navbar.Brand className="collapse navbar-collapse" id="brand-name">
+            <a className="nav-link" href="#brand">
+              <img
+                src={logoLg}
+                srcSet={`${logo} 1080w, ${logoLg} 1440w`}
+                sizes="(max-width: 1080px) 100vw, 1440px"
+                alt="logo"
+                className="pull-left visible-md visible-lg"
+              />
+            </a>
+          </Navbar.Brand>
+
+          <div className="navbar-text">
+            <h2 className={`${styles.navbarHeader} navbar-header h2`}>
+              DAVID CHU'S CHINA BISTRO
+            </h2>
+            <div className="d-flex flex-row align-items-center">
+              <span className="me-2 navbar-header">
+                <img src={star} alt="start-k-logo" />
+              </span>
+              KOSHER CERTIFIED
+            </div>
           </div>
         </div>
-        <div>
-          {isExpanded && (
-            <ul
-              className={`${styles.list} d-flex flex-d-row align-items-center p-0 m-0`}
-            >
-              <li>Menu</li>
-              <li>About</li>
-              <li>Awards</li>
-              <li>
-                <h3 className="m-0">410 602 5008</h3>* We Deliver
-              </li>
-            </ul>
-          )}
-        </div>
+
         <div>
           <button
             className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
-            data-bs-target="#navbarToggleExternalContent"
-            aria-controls="navbarToggleExternalContent"
-            aria-expanded="true"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
+        </div>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto">
+            <li className="nav-item">
+              <a
+                className="nav-link acitve visible-xs"
+                aria-current="page"
+                href="#home"
+              >
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#menu">
+                Menu
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#about">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#awards">
+                Awards
+              </a>
+            </li>
+            <li className="nav-item ms-2">
+              410-602-5008
+              <div>* We Deliver</div>
+            </li>
+          </ul>
         </div>
       </Container>
     </Navbar>
