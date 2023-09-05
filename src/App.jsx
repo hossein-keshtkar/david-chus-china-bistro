8abde2ffa254e78bbb8b0e6f55b4d7868b00bd50
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-09-05 03:58:02 -0400

    working on lazyloading

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import styles from "./styles/App.module.css";
 import Navbar from "./components/Navbar";
+import MainImg from "./components/MainImg";
 
 function App() {
   return (
     <div className={styles.App}>
       <Navbar />
+      <MainImg />
     </div>
   );
 }
---
+++ src/components/MainImg.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import { Container, Image } from "react-bootstrap";
+
+import imgLg from "../assets/images/jumbotron_1200.jpg";
+import imgMd from "../assets/images/jumbotron_992.jpg";
+import imgSm from "../assets/images/jumbotron_768.jpg";
+import bluryImg from "../assets/images/blury-resturant.png";
+import styles from "../styles/MainImg.module.css";
+
+const MainImg = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const handleLoad = () => {};
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoaded((prev) => !prev);
+    }, 2000);
+  }, []);
+
+  return (
+    <Container className="d-flex align-items-center justify-content-center">
+      {isLoaded ? (
+        <Image
+          rounded
+          src={imgSm}
+          srcSet={`${imgSm} 768w, ${imgMd} 992w, ${imgLg} 1440w`}
+          sizes="(max-width: 768px) 100vw, (max-width: 992px) 100vw, 90vw"
+          alt="resturaunt"
+          className={`${styles.MainImg} img-fluid mt-3`}
+        />
+      ) : (
+        <Image
+          rounded
+          src={bluryImg}
+          alt="blury-resturaunt"
+          className={`${styles.bluryImg} img-fluid mt-3`}
+        />
+      )}
+    </Container>
+  );
+};
+
+export default MainImg;
