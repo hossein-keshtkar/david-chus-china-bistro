95e9218143012d91cf855a02a65cab01ad132bc6
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-09-12 00:39:51 -0400

    BottomSection's UI improved

--- src/components/home/BottomSection.jsx
+++ src/components/home/BottomSection.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Container, Image } from "react-bootstrap";
 
 import menuImg from "../../assets/images/menu-tile.jpg";
@@ -7,23 +7,46 @@ import styles from "../../styles/BottomSection.module.css";
 import { Link } from "react-router-dom";
 
 const HomePageBottomSection = () => {
+  const [isMapLoaded, setIsMapLoaded] = useState(false);
+  const mapRef = useRef();
+
+  const mapLoadHandler = () => {
+    setIsMapLoaded(true);
+  };
+
+  useEffect(() => {
+    // mapRef.current
+    // mapRef.onLoad = () => {
+    //   setIsMapLoaded(true);
+    // };
+  }, []);
+
+  useEffect(() => {
+    console.log(isMapLoaded);
+  }, [isMapLoaded]);
+
   return (
     <Container className="my-5">
       <div className={`${styles.row} row`}>
-        <div className="col-lg-4 col-md-6 col-sm-6 col-12 mb-3 d-flex align-items-center justify-content-center">
-          <Link to="/menu">
-            <Image src={menuImg} className={styles.images} loading="lazy" />
-          </Link>
-        </div>
-        <div className="col-lg-4 col-md-6 col-sm-6 col-12 mb-3 d-flex align-items-center justify-content-center">
-          <Link to="/menu/specials">
-            <Image src={specialsImg} className={styles.images} loading="lazy" />
-          </Link>
-        </div>
+        <Link
+          to="/menu"
+          className="col-lg-4 col-md-6 col-sm-6 col-12 mb-3 d-flex align-items-center justify-content-center"
+        >
+          <Image src={menuImg} className={styles.images} loading="lazy" />
+        </Link>
+
+        <Link
+          to="/menu/specials"
+          className="col-lg-4 col-md-6 col-sm-6 col-12 mb-3 d-flex align-items-center justify-content-center"
+        >
+          <Image src={specialsImg} className={styles.images} loading="lazy" />
+        </Link>
+
         <div className="col-lg-4 col-md-12 mb-3 d-flex align-items-center justify-content-center">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3084.675063579344!2d-76.71426302412522!3d39.36359441906242!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c81a14e7817803%3A0xab20a0e99daa17ea!2sDavid%20Chu&#39;s%20China%20Bistro!5e0!3m2!1sen!2snl!4v1694054215589!5m2!1sen!2snl"
             allowFullScreen=""
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3084.675063579344!2d-76.71426302412522!3d39.36359441906242!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c81a14e7817803%3A0xab20a0e99daa17ea!2sDavid%20Chu&#39;s%20China%20Bistro!5e0!3m2!1sen!2snl!4v1694054215589!5m2!1sen!2snl"
+            ref={mapRef}
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
             className={styles.map}
--- src/components/home/MainImg.jsx
+++ src/components/home/MainImg.jsx
@@ -5,7 +5,6 @@ import imgLg from "../../assets/images/jumbotron_1200.jpg";
 import imgMd from "../../assets/images/jumbotron_992.jpg";
 import imgSm from "../../assets/images/jumbotron_768.jpg";
 import styles from "../../styles/MainImg.module.css";
-import blurred from "../../assets/images/blury-resturant.png";
 
 const MainImg = () => {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -18,18 +17,16 @@ const MainImg = () => {
 
     imgRef.current.addEventListener("load", handleImgLoad);
 
-    return () => {
-      imgRef.current.removeEventListener("load", handleImgLoad);
-    };
+    return () => imgRef.current.removeEventListener("load", handleImgLoad);
   }, []);
 
   return (
     <Container className="d-flex align-items-center justify-content-center my-3">
       <div className={`${styles.blurredImg} ${isLoaded ? styles.loaded : ""}`}>
         <Image
-          src={imgSm}
+          // src={imgSm}
           ref={imgRef}
-          srcSet={`${imgSm} 768w, ${imgMd} 992w, ${imgLg} 1440w`}
+          // srcSet={`${imgSm} 768w, ${imgMd} 992w, ${imgLg} 1440w`}
           sizes="(max-width: 768px) 100vw, (max-width: 992px) 100vw, 90vw"
           alt="resturaunt"
           loading="lazy"
