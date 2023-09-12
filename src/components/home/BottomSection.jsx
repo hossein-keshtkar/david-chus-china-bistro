6fbf4f1d5675b38d51832f0178b96f377e891d2d
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-09-12 06:01:17 -0400

    BottomSection looks good

--- src/components/home/BottomSection.jsx
+++ src/components/home/BottomSection.jsx
@@ -8,7 +8,13 @@ import { Link } from "react-router-dom";
 
 const HomePageBottomSection = () => {
   const [isMapLoaded, setIsMapLoaded] = useState(false);
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+  const [isSpecialsVisible, setIsSpecialsVisible] = useState(false);
+  const [isMapHovered, setIsMapHovered] = useState(false);
   const mapRef = useRef();
+
+  const className =
+    "col-lg-4 col-md-6 col-sm-6 col-12 mb-3 d-flex align-items-center justify-content-center";
 
   const mapLoadHandler = () => {
     setIsMapLoaded(true);
@@ -21,36 +27,88 @@ const HomePageBottomSection = () => {
     // };
   }, []);
 
-  useEffect(() => {
-    console.log(isMapLoaded);
-  }, [isMapLoaded]);
+  // useEffect(() => {
+  //   console.log(isMapLoaded);
+  // }, [isMapLoaded]);
 
   return (
     <Container className="my-5">
-      <div className={`${styles.row} row`}>
-        <Link
-          to="/menu"
-          className="col-lg-4 col-md-6 col-sm-6 col-12 mb-3 d-flex align-items-center justify-content-center"
-        >
-          <Image src={menuImg} className={styles.images} loading="lazy" />
-        </Link>
+      <div className="row">
+        <div className={className}>
+          <Link to="/menu" className={styles.links}>
+            <Image
+              onMouseEnter={() => {
+                setIsMenuVisible(true);
+              }}
+              onMouseLeave={() => {
+                setIsMenuVisible(false);
+              }}
+              src={menuImg}
+              className={styles.images}
+              loading="lazy"
+              rounded
+            />
+            <h1
+              className={styles.description}
+              style={{
+                opacity: isMenuVisible ? 1 : 0,
+              }}
+            >
+              Menu
+            </h1>
+          </Link>
+        </div>
 
-        <Link
-          to="/menu/specials"
-          className="col-lg-4 col-md-6 col-sm-6 col-12 mb-3 d-flex align-items-center justify-content-center"
-        >
-          <Image src={specialsImg} className={styles.images} loading="lazy" />
-        </Link>
+        <div className={className}>
+          <Link to="/menu/specials" className={styles.links}>
+            <Image
+              onMouseEnter={() => {
+                setIsSpecialsVisible(true);
+              }}
+              onMouseLeave={() => {
+                setIsSpecialsVisible(false);
+              }}
+              src={specialsImg}
+              className={styles.images}
+              loading="lazy"
+              rounded
+            />
+            <h1
+              className={styles.description}
+              style={{
+                opacity: isSpecialsVisible ? 1 : 0,
+              }}
+            >
+              Specials
+            </h1>
+          </Link>
+        </div>
 
-        <div className="col-lg-4 col-md-12 mb-3 d-flex align-items-center justify-content-center">
-          <iframe
-            allowFullScreen=""
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3084.675063579344!2d-76.71426302412522!3d39.36359441906242!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c81a14e7817803%3A0xab20a0e99daa17ea!2sDavid%20Chu&#39;s%20China%20Bistro!5e0!3m2!1sen!2snl!4v1694054215589!5m2!1sen!2snl"
-            ref={mapRef}
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-            className={styles.map}
-          ></iframe>
+        <div className="col-lg-4 col-md-12 mb-3">
+          <div className="position-relative h-100 w-100 d-flex justify-content-center">
+            <iframe
+              allowFullScreen={false}
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3084.675063579344!2d-76.71426302412522!3d39.36359441906242!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c81a14e7817803%3A0xab20a0e99daa17ea!2sDavid%20Chu&#39;s%20China%20Bistro!5e0!3m2!1sen!2snl!4v1694054215589!5m2!1sen!2snl"
+              ref={mapRef}
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+              className={`${styles.map} rounded`}
+              onMouseEnter={() => {
+                setIsMapHovered(true);
+              }}
+              onMouseLeave={() => {
+                setIsMapHovered(false);
+              }}
+            ></iframe>
+            <h1
+              className={styles.description}
+              style={{
+                opacity: isMapHovered ? 1 : 0,
+              }}
+            >
+              Location
+            </h1>
+          </div>
         </div>
       </div>
     </Container>
