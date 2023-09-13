6fbaef3457c4801b0223cfc7d6ad60d572e2331f
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-09-13 09:06:23 -0400

    LazyImage in progress

--- src/components/LazyImage.jsx
+++ src/components/LazyImage.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { Image } from "react-bootstrap";
 
-const LazyImage = () => {
-  return <div>LazyImage</div>;
+const LazyImage = ({ blurred, src, alt }) => {
+  const [isMainImgLoaded, setIsMainImgLoaded] = useState(false);
+
+  const style = {
+    aspectRatio: "2/1.2",
+  };
+
+  const imgRef = useRef();
+
+  return (
+    <div className="position-relative">
+      <Image
+        className="w-100 rounded position-absolute"
+        ref={imgRef}
+        src={blurred}
+        alt={alt}
+        style={style}
+      />
+      <Image
+        className="w-100 rounded"
+        src={src}
+        alt={alt}
+        style={style}
+        onLoad={() => {
+          setIsMainImgLoaded(true);
+        }}
+      />
+    </div>
+  );
 };
 
 export default LazyImage;
--- src/components/menu/MenuPage.jsx
+++ src/components/menu/MenuPage.jsx
@@ -3,11 +3,14 @@ import MyNavbar from "../Navbar";
 import Footer from "../Footer";
 import LazyImage from "../LazyImage";
 
+import blurredMenu from "../../assets/images/blurred-menu.png";
+import menu from "../../assets/images/menu-tile.jpg";
+
 const Menu = () => {
   return (
     <div>
       <MyNavbar />
-      <LazyImage />
+      <LazyImage src={menu} blurred={blurredMenu} alt={"image"} />
       <Footer />
     </div>
   );
