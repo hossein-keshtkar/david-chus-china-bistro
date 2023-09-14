47f91c548366aeb5e943db37e6696ddd753e5cba
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-09-14 15:20:44 -0400

    LinkItem completed

--- src/components/home/LinkItem.jsx
+++ src/components/home/LinkItem.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState, memo } from "react";
+import { Link } from "react-router-dom";
 
-const LinkItem = () => {
-  return <div>LinkItem</div>;
+import styles from "../../styles/LinkItem.module.css";
+import LazyImg from "../LazyImg";
+
+const LinkItem = ({ to, src, blurred, alt, header }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <div className={`${styles.item} col-lg-4 col-md-6 col-sm-6 col-12 mb-3`}>
+      <Link
+        to={to}
+        onMouseEnter={() => {
+          setIsHovered(true);
+        }}
+        onMouseLeave={() => {
+          setIsHovered(false);
+        }}
+      >
+        <LazyImg src={src} blurred={blurred} alt={alt} />
+        <h1
+          className={`${styles.description} d-none d-lg-block`}
+          style={{
+            opacity: isHovered ? 1 : 0,
+          }}
+        >
+          {header}
+        </h1>
+      </Link>
+    </div>
+  );
 };
 
-export default LinkItem;
+export default memo(LinkItem);
