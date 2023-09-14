6ee8484cce816c44e7be8983cc4a7ed48ccfdd15
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-09-14 15:21:17 -0400

    extra imports removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import { RouterProvider } from "react-router-dom";
-import { Suspense, lazy } from "react";
+import { Suspense } from "react";
 
 import styles from "./styles/App.module.css";
-import Navbar from "./components/Navbar";
 import { router } from "./components/router/router";
 import FallBack from "./components/FallBack";
-
-const Footer = lazy(() => import("./components/Footer"));
 
 function App() {
   return (
