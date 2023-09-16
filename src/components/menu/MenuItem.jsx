0d38ba8fb4d985fd309a3c1243b9cdc4a414b94c
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-09-16 10:26:00 -0400

    working on MenuItem

---
+++ src/components/menu/MenuItem.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "../../styles/MenuPage.module.css";
+
+const MenuItem = () => {
+  return (
+    <div className={`${styles.item} col-lg-3 col-md-4 mb-3`}>
+      <div className="w-100 h-100 border border-dark p-2">hey</div>
+    </div>
+  );
+};
+
+export default MenuItem;
