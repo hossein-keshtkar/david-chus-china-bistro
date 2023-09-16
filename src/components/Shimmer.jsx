24cbd7f96a45c556ba5f0ee02f0e291a098cb2f4
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-09-16 14:08:12 -0400

    Shimmer added

---
+++ src/components/Shimmer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "../styles/Shimmer.module.css";
+
+const Shimmer = () => {
+  return <div className={styles.shimmer}></div>;
+};
+
+export default Shimmer;
