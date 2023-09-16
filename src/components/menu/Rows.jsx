ac303591dd2a2eb1372fa9b94d5dbba443fb365e
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-09-16 10:25:36 -0400

    Changed Row to Rows

---
+++ src/components/menu/Rows.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import MenuItem from "./MenuItem";
+
+const Rows = () => {
+  return (
+    <div className="row">
+      <MenuItem />
+      <MenuItem />
+      <MenuItem />
+      <MenuItem />
+      <MenuItem />
+      <MenuItem />
+      <MenuItem />
+      <MenuItem />
+    </div>
+  );
+};
+
+export default Rows;
