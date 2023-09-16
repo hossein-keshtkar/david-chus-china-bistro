697a4b6a110648ecb98dd85c277a8a3bc58546fa
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-09-16 14:30:38 -0400

    Location improved a bit

--- src/components/home/Location.jsx
+++ src/components/home/Location.jsx
@@ -8,27 +8,32 @@ const Location = () => {
   const [isMapLoaded, setIsMapLoaded] = useState(false);
   const mapRef = useRef();
 
+  const handleMouseEnter = () => {
+    setIsMapHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsMapHovered(false);
+  };
+
+  const handleOnLoad = () => {
+    setIsMapLoaded(true);
+  };
+
   return (
     <div className="position-relative h-100 w-100 d-flex flex-column justify-content-center">
       <iframe
         className={`${styles.map} rounded`}
-        allowFullScreen={false}
-        title="google-map-david-chu\'s-china-bistro"
-        onLoad={() => {
-          setIsMapLoaded(true);
-        }}
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3084.675063579344!2d-76.71426302412522!3d39.36359441906242!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c81a14e7817803%3A0xab20a0e99daa17ea!2sDavid%20Chu&#39;s%20China%20Bistro!5e0!3m2!1sen!2snl!4v1694054215589!5m2!1sen!2snl"
-        ref={mapRef}
-        loading="lazy"
+        title="google-map-david-chu\'s-china-bistro"
         referrerPolicy="no-referrer-when-downgrade"
-        onMouseEnter={() => {
-          setIsMapHovered(true);
-        }}
-        onMouseLeave={() => {
-          setIsMapHovered(false);
-        }}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        allowFullScreen={false}
+        onLoad={handleOnLoad}
+        loading="lazy"
+        ref={mapRef}
       ></iframe>
-      {!isMapLoaded && <Shimmer />}
       <h1
         className={`${styles.description} d-none d-lg-block`}
         style={{
@@ -37,6 +42,9 @@ const Location = () => {
       >
         Location
       </h1>
+      {!isMapLoaded && (
+        <Shimmer style={{ aspectRatio: "2/1.2", height: "auto" }} />
+      )}
     </div>
   );
 };
