cf174d467ce885510b8f38cbf98aa7ec40e6c637
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-09-18 11:24:47 -0400

    working on menuCategories

--- src/components/menu/MenuRows.jsx
+++ src/components/menu/MenuRows.jsx
@@ -2,22 +2,19 @@ import React from "react";
 
 import MenuItem from "./MenuItem";
 
-import blurred from "../../assets/images/menu/Blurred/A/download.png";
-import img from "../../assets/images/menu/A/A.jpg";
-import { Container } from "react-bootstrap";
+import { menuCategories } from "../../data/menuCategories";
 
 const MenuRows = () => {
   return (
     <div className="row my-5">
-      <MenuItem bgImg={blurred} img={img} title={"SOUP"} />
-      <MenuItem />
-      <MenuItem />
-      <MenuItem />
-      <MenuItem />
-      <MenuItem />
-      <MenuItem />
-      <MenuItem />
-      <MenuItem />
+      {menuCategories.map((item) => (
+        <MenuItem
+          bgImg={item.bgImg}
+          img={item.mainImg}
+          title={item.title}
+          key={item.id}
+        />
+      ))}
     </div>
   );
 };
---
+++ src/data/menuCategories.js
@@ -0,0 +1,25 @@
+import soupBg from "../assets/images/menu/bgImg/soup/A.png";
+import soupImg from "../assets/images/menu/mainImg/soup/A.jpg";
+import lunchBg from "../assets/images/menu/bgImg/lunch/L.png";
+import lunchImg from "../assets/images/menu/mainImg/lunch/L.jpg";
+
+export const menuCategories = [
+  {
+    title: "SOUP",
+    id: "soup",
+    bgImg: soupBg,
+    mainImg: soupImg,
+  },
+  {
+    title: "LUNCH",
+    id: "lunch",
+    bgImg: lunchBg,
+    mainImg: lunchImg,
+  },
+  {
+    title: "APPETIZER",
+    id: "appetizer",
+    bgImg: lunchBg,
+    mainImg: lunchImg,
+  },
+];
