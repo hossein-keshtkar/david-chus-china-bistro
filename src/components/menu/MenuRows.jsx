b31426fd56f5f1a636c4fbc045f8492462c98742
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-09-19 09:42:16 -0400

    adjusting MenuRows compared to MenuItem

--- src/components/menu/MenuRows.jsx
+++ src/components/menu/MenuRows.jsx
@@ -1,19 +1,28 @@
 import React from "react";
 
 import MenuItem from "./MenuItem";
-
 import { menuCategories } from "../../data/menuCategories";
+import styles from "../../styles/MenuRows.module.css";
 
 const MenuRows = () => {
+  const menu = process.env.REACT_APP_MENU_API;
+
   return (
     <div className="row my-5">
       {menuCategories.map((item) => (
-        <MenuItem
-          bgImg={item.bgImg}
-          img={item.mainImg}
-          title={item.title}
-          key={item.id}
-        />
+        <div
+          className={`${styles.container} col-lg-3 col-md-4 d-flex align-items-center justify-content-center`}
+        >
+          <MenuItem
+            bg={item.bgImg}
+            img={item.mainImg}
+            title={item.title}
+            key={item.id}
+            to={`${menu}/${item.id}`}
+            height={200}
+            width={200}
+          />
+        </div>
       ))}
     </div>
   );
