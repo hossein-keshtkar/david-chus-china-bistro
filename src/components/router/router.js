b11f3bdef0d9c3499cb9ec5973c6e6f4a3e923ba
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-09-19 06:48:06 -0400

    local apis imported

--- src/components/router/router.js
+++ src/components/router/router.js
@@ -8,26 +8,32 @@ const Menu = lazy(() => import("../menu/MenuPage"));
 const Specials = lazy(() => import("../specials/SpecialsPage"));
 const Products = lazy(() => import("../menu/Products"));
 
+const homeApi = process.env.REACT_APP_HOME_API;
+const loginApi = process.env.REACT_APP_ADMIN_API;
+const menuApi = process.env.REACT_APP_MENU_API;
+const specialsApi = process.env.REACT_APP_SPECIALS_API;
+const productsApi = process.env.REACT_APP_PRODUCT_API;
+
 export const router = createBrowserRouter([
   {
-    path: "/",
+    path: homeApi,
     element: <Home />,
   },
   {
-    path: "/login/admin",
+    path: loginApi,
     element: <Login />,
   },
   {
-    path: "/menu",
+    path: menuApi,
     element: <Menu />,
   },
   {
-    path: "/menu/specials",
+    path: specialsApi,
     element: <Specials />,
     errorElement: <Error />,
   },
   {
-    path: "/menu/:id",
+    path: productsApi,
     element: <Products />,
     errorElement: <Error />,
   },
