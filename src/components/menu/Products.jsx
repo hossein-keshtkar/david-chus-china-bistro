5f5305a2434fd694ea6a6cdc1e646389043f86c9
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-09-19 13:32:02 -0400

    working on Products

--- src/components/menu/Products.jsx
+++ src/components/menu/Products.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 
 import Page from "../Page";
+import { lunchMenu } from "../../data/lunchMenu";
+import ProductsItem from "./ProductsItem";
 
-const Products = ({ description, header, label, price, img, bg }) => {
-  return <Page>Products</Page>;
+const Products = (params) => {
+  console.log(params);
+  
+  return (
+    <Page>
+      <Container>
+        <div className="row">
+          {lunchMenu.map((item, index) => (
+            <div className="col-lg-6 col-md-12 mb-3">
+              <ProductsItem
+                description={item.description}
+                header={item.header}
+                label={item.label}
+                price={item.price}
+                img={item.img}
+                bg={item.bg}
+                key={index}
+              />
+            </div>
+          ))}
+        </div>
+      </Container>
+    </Page>
+  );
 };
 
 export default Products;
