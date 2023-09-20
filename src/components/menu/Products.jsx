51d57571365e37fc72a75cd83ecbe9ead7084de0
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-09-20 15:15:25 -0400

    Products is dynamic now

--- src/components/menu/Products.jsx
+++ src/components/menu/Products.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 
 import Page from "../Page";
 import { lunchMenu } from "../../data/lunchMenu";
 import ProductsItem from "./ProductsItem";
 
-const Products = (params) => {
-  console.log(params);
-  
+const Products = () => {
+  const { id } = useParams();
+
+  const data = lunchMenu.find((item) => item.id === id);
+  const { items } = data;
+
   return (
     <Page>
       <Container>
+        <div className="my-5 text-center">
+          <h3>{data.header}</h3>
+          <p>{data.paragraph}</p>
+        </div>
         <div className="row">
-          {lunchMenu.map((item, index) => (
-            <div className="col-lg-6 col-md-12 mb-3">
+          {items.map((item, index) => (
+            <div key={index} className="col-lg-6 col-md-12 mb-3">
               <ProductsItem
                 description={item.description}
                 header={item.header}
@@ -21,7 +29,6 @@ const Products = (params) => {
                 price={item.price}
                 img={item.img}
                 bg={item.bg}
-                key={index}
               />
             </div>
           ))}
