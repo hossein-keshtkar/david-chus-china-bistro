fd9323a7104c04bb325c12e2eda291d2ff5a2f99
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-09-21 14:13:09 -0400

    lunchMenu completed

--- src/data/lunchMenu.js
+++ src/data/lunchMenu.js
@@ -16,100 +16,284 @@ import l17Img from "../assets/images/menu/mainImg/lunch/L17.jpg";
 import l17Bg from "../assets/images/menu/bgImg/lunch/L17.png";
 import l15Img from "../assets/images/menu/mainImg/lunch/L15.jpg";
 import l15Bg from "../assets/images/menu/bgImg/lunch/L15.png";
+import l14Img from "../assets/images/menu/mainImg/lunch/L14.jpg";
+import l14Bg from "../assets/images/menu/bgImg/lunch/L14.png";
+import l16Img from "../assets/images/menu/mainImg/lunch/L16.jpg";
+import l16Bg from "../assets/images/menu/bgImg/lunch/L16.png";
+import l22Img from "../assets/images/menu/mainImg/lunch/L22.jpg";
+import l22Bg from "../assets/images/menu/bgImg/lunch/L22.png";
+import l28Img from "../assets/images/menu/mainImg/lunch/L28.jpg";
+import l28Bg from "../assets/images/menu/bgImg/lunch/L28.png";
+import l4Img from "../assets/images/menu/mainImg/lunch/L4.jpg";
+import l4Bg from "../assets/images/menu/bgImg/lunch/L4.png";
+import l11Img from "../assets/images/menu/mainImg/lunch/L11.jpg";
+import l11Bg from "../assets/images/menu/bgImg/lunch/L11.png";
+import l13Img from "../assets/images/menu/mainImg/lunch/L13.jpg";
+import l13Bg from "../assets/images/menu/bgImg/lunch/L13.png";
+import l2Img from "../assets/images/menu/mainImg/lunch/L2.jpg";
+import l2Bg from "../assets/images/menu/bgImg/lunch/L2.png";
+import l23Img from "../assets/images/menu/mainImg/lunch/L23.jpg";
+import l23Bg from "../assets/images/menu/bgImg/lunch/L23.png";
+import l26Img from "../assets/images/menu/mainImg/lunch/L26.jpg";
+import l26Bg from "../assets/images/menu/bgImg/lunch/L26.png";
+import l6Img from "../assets/images/menu/mainImg/lunch/L6.jpg";
+import l6Bg from "../assets/images/menu/bgImg/lunch/L6.png";
+import l5Img from "../assets/images/menu/mainImg/lunch/L5.jpg";
+import l5Bg from "../assets/images/menu/bgImg/lunch/L5.png";
+import l24Img from "../assets/images/menu/mainImg/lunch/L24.jpg";
+import l24Bg from "../assets/images/menu/bgImg/lunch/L24.png";
+import l7Img from "../assets/images/menu/mainImg/lunch/L7.jpg";
+import l7Bg from "../assets/images/menu/bgImg/lunch/L7.png";
+import l3Img from "../assets/images/menu/mainImg/lunch/L3.jpg";
+import l3Bg from "../assets/images/menu/bgImg/lunch/L3.png";
+import l20Img from "../assets/images/menu/mainImg/lunch/L20.jpg";
+import l20Bg from "../assets/images/menu/bgImg/lunch/L20.png";
+import l21Img from "../assets/images/menu/mainImg/lunch/L21.jpg";
+import l21Bg from "../assets/images/menu/bgImg/lunch/L21.png";
+import l18Img from "../assets/images/menu/mainImg/lunch/L18.jpg";
+import l18Bg from "../assets/images/menu/bgImg/lunch/L18.png";
 
-export const lunchMenu = [
-  {
-    id: "lunch",
-    header: "Lunch Menu",
-    paragraph:
-      "Sunday-Friday 11:15am-3:00pm. Served with your choice of rice (Vegetable Fried RIce, Steamed Rice, Brown Rice), AND EITHER soup (Hot & Sour, Wonton, Vegetable, Egg Drop, Chicken Corn Soup) OR veggie egg roll.",
-    items: [
-      {
-        description:
-          "white meat chicken sauteed with carrots, celery, and bean sprouts in Szechuan sauce",
-        header: "Szechuan Chicken",
-        label: "L12",
-        price: "$13.55",
-        img: l12Img,
-        bg: l12Bg,
-      },
-      {
-        description:
-          "white meat chicken, breaded and fried with some green pepper, onion, and pineapples",
-        header: "Sweet and Sour Chicken",
-        label: "L9",
-        price: "$13.55",
-        img: l9Img,
-        bg: l9Bg,
-      },
-      {
-        description:
-          "chunks of chicken, breaded and deep-fried with sauce containing orange peels; white meat by request, $1 more for white meat",
-        header: "Orange Chicken",
-        label: "L1",
-        price: "$13.55",
-        img: l1Img,
-        bg: l1Bg,
-      },
-      {
-        description:
-          "white meat chicken OR beef OR vegetables, sauteed with onions, and bean sprouts and soft lo mein noodles",
-        header: "Lo Mein",
-        label: "L27",
-        price: "$13.55",
-        img: l27Img,
-        bg: l27Bg,
-      },
-      {
-        description:
-          "bean curd lightly fried then sauteed with green pepper, snow peas, and water chestnuts in brown sauce",
-        header: "Hunan Bean Curd",
-        label: "L19",
-        price: "$13.55",
-        img: l19Img,
-        bg: l19Bg,
-      },
-      {
-        description:
-          "white meat chicken, string beans, waterchestnuts, mushrooms, and eggplant, in garlic sauce",
-        header: "Chicken Eggplant with Garlic Sauce",
-        label: "L8",
-        price: "$13.55",
-        img: l8Img,
-        bg: l8Bg,
-      },
-      {
-        description: "white meat chicken sauteed with broccoli in brown sauce",
-        header: "Chicken Broccoli",
-        label: "L10",
-        price: "$13.55",
-        img: l10Img,
-        bg: l10Bg,
-      },
-      {
-        description:
-          "sliced beef sauteed with green peppers and onions in brown sauce",
-        header: "Beef with Green Pepper",
-        label: "L17",
-        price: "$13.55",
-        img: l17Img,
-        bg: l17Bg,
-      },
-      {
-        description: "sliced beef sauteed in garlic sauce",
-        header: "Beef with Garlic Sauce",
-        label: "L15",
-        price: "$13.55",
-        img: l15Img,
-        bg: l15Bg,
-      },
-    ],
-  },
-  {
-    id: "soup",
-    header: "Soup Menu",
-    paragraph: "",
-    header: "",
-    items: [],
-  },
-];
+export const lunchMenu = {
+  id: "lunch",
+  header: "Lunch Menu",
+  paragraph:
+    "Sunday-Friday 11:15am-3:00pm. Served with your choice of rice (Vegetable Fried RIce, Steamed Rice, Brown Rice), AND EITHER soup (Hot & Sour, Wonton, Vegetable, Egg Drop, Chicken Corn Soup) OR veggie egg roll.",
+  items: [
+    {
+      description:
+        "white meat chicken sauteed with carrots, celery, and bean sprouts in Szechuan sauce",
+      header: "Szechuan Chicken",
+      label: "L12",
+      price: "$13.55",
+      img: l12Img,
+      bg: l12Bg,
+    },
+    {
+      description:
+        "white meat chicken, breaded and fried with some green pepper, onion, and pineapples",
+      header: "Sweet and Sour Chicken",
+      label: "L9",
+      price: "$13.55",
+      img: l9Img,
+      bg: l9Bg,
+    },
+    {
+      description:
+        "chunks of chicken, breaded and deep-fried with sauce containing orange peels; white meat by request, $1 more for white meat",
+      header: "Orange Chicken",
+      label: "L1",
+      price: "$13.55",
+      img: l1Img,
+      bg: l1Bg,
+    },
+    {
+      description:
+        "white meat chicken OR beef OR vegetables, sauteed with onions, and bean sprouts and soft lo mein noodles",
+      header: "Lo Mein",
+      label: "L27",
+      price: "$13.55",
+      img: l27Img,
+      bg: l27Bg,
+    },
+    {
+      description:
+        "bean curd lightly fried then sauteed with green pepper, snow peas, and water chestnuts in brown sauce",
+      header: "Hunan Bean Curd",
+      label: "L19",
+      price: "$13.55",
+      img: l19Img,
+      bg: l19Bg,
+    },
+    {
+      description:
+        "white meat chicken, string beans, waterchestnuts, mushrooms, and eggplant, in garlic sauce",
+      header: "Chicken Eggplant with Garlic Sauce",
+      label: "L8",
+      price: "$13.55",
+      img: l8Img,
+      bg: l8Bg,
+    },
+    {
+      description: "white meat chicken sauteed with broccoli in brown sauce",
+      header: "Chicken Broccoli",
+      label: "L10",
+      price: "$13.55",
+      img: l10Img,
+      bg: l10Bg,
+    },
+    {
+      description:
+        "sliced beef sauteed with green peppers and onions in brown sauce",
+      header: "Beef with Green Pepper",
+      label: "L17",
+      price: "$13.55",
+      img: l17Img,
+      bg: l17Bg,
+    },
+    {
+      description: "sliced beef sauteed in garlic sauce",
+      header: "Beef with Garlic Sauce",
+      label: "L15",
+      price: "$13.55",
+      img: l15Img,
+      bg: l15Bg,
+    },
+    {
+      description: "sliced beef sauteed with broccoli in brown sauce",
+      header: "Beef with Broccoli",
+      label: "L14",
+      price: "$13.55",
+      img: l14Img,
+      bg: l14Bg,
+    },
+    {
+      description: "sliced beef sauteed with string beans and onions",
+      header: "Beef String Bean",
+      label: "L16",
+      price: "$13.55",
+      img: l16Img,
+      bg: l16Bg,
+    },
+    {
+      description: "onions and string beans sauteed with chef's sauce",
+      header: "Szechuan String Bean",
+      label: "L22",
+      price: "$13.55",
+      img: l22Img,
+      bg: l22Bg,
+    },
+    {
+      description:
+        "chunks of chicken, breaded and deep-fried with a sesame seed sauce. White meat $1 extra",
+      header: "Sesame Chicken",
+      label: "L28",
+      price: "$13.55",
+      img: l28Img,
+      bg: l28Bg,
+    },
+    {
+      description:
+        "Dark meat chicken sauteed with carrots and celery, in a spicy Szechuan sauce, $1 more for white meat chicken",
+      header: "Kung Pao Chicken",
+      label: "L4",
+      price: "$13.55",
+      img: l4Img,
+      bg: l4Bg,
+    },
+    {
+      description:
+        "white meat chicken sauteed with broccoli, mushrooms, and baby corn in Hunan sauce",
+      header: "Hunan Chicken",
+      label: "L11",
+      price: "$13.55",
+      img: l11Img,
+      bg: l11Bg,
+    },
+    {
+      description:
+        "beef sauteed with broccoli, mushrooms, and carrots in spicy Hunan sauce",
+      header: "Hunan Beef",
+      label: "L13",
+      price: "$13.55",
+      img: l13Img,
+      bg: l13Bg,
+    },
+    {
+      description:
+        "hunks of chicken, breaded and deep-fried with sauce and scallions; white meat by request, $1 extra for white meat",
+      header: "General Tso's Chicken",
+      label: "L2",
+      price: "$13.55",
+      img: l2Img,
+      bg: l2Bg,
+    },
+    {
+      description:
+        "eggplant sauteed with water chestnuts and string beans in garlic sauce",
+      header: "Eggplant with Garlic Sauce",
+      label: "L23",
+      price: "$13.55",
+      img: l23Img,
+      bg: l23Bg,
+    },
+    {
+      description:
+        "white meat chicken OR beef OR vegetables, sauteed with bean sprouts, onions, and celery in white sauce",
+      header: "Chow Mein",
+      label: "L26",
+      price: "$13.55",
+      img: l26Img,
+      bg: l26Bg,
+    },
+    {
+      description:
+        "white meat chicken in a clear white sauce sauteed with mixed vegetables",
+      header: "Chicken Vegetable",
+      label: "L6",
+      price: "$13.55",
+      img: l6Img,
+      bg: l6Bg,
+    },
+    {
+      description: "white meat chicken sauteed with string beans and soy sauce",
+      header: "Chicken String Bean",
+      label: "L5",
+      price: "$13.55",
+      img: l5Img,
+      bg: l5Bg,
+    },
+    {
+      description: "white meat chicken sauteed with onions and bean sprouts",
+      header: "Chicken Mei Fan",
+      label: "L24",
+      price: "$13.55",
+      img: l24Img,
+      bg: l24Bg,
+    },
+    {
+      description:
+        "chicken sauteed with string beans, mushrooms, and waterchestnuts in garlic sauce",
+      header: "Chicken Garlic Sauce",
+      label: "L7",
+      price: "$13.55",
+      img: l7Img,
+      bg: l7Bg,
+    },
+    {
+      description:
+        "diced chicken with waterchestnuts, green peppers, and celery, and cashewnuts; white meat by request, $1 extra for white meat",
+      header: "Chicken Cashewnuts",
+      label: "L3",
+      price: "$13.55",
+      img: l3Img,
+      bg: l3Bg,
+    },
+    {
+      description:
+        "Tofu lightly breaded and fried and sauteed with carrots, broccoli, snow peas, string beans, water chestnuts, and mushrooms in brown sauce",
+      header: "Bean Curd with Vegetables",
+      label: "L20",
+      price: "$13.55",
+      img: l20Img,
+      bg: l20Bg,
+    },
+    {
+      description:
+        "broccoli, carrots, baby corn, water chestnuts, mushrooms, and snow peas sauteed in brown sauce",
+      header: "Wok's Mixed Vegetables",
+      label: "L21",
+      price: "$13.55",
+      img: l21Img,
+      bg: l21Bg,
+    },
+    {
+      description:
+        "sliced veal sauteed with carrots, broccoli, snow peas, string beans, water chestnuts, and mushrooms in brown sauce",
+      header: "Veal with Mixed Vegetables",
+      label: "L18",
+      price: "$13.55",
+      img: l18Img,
+      bg: l18Bg,
+    },
+  ],
+};
