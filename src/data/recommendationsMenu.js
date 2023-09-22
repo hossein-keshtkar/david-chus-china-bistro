02516c1096e08090d61db68da680a9450e5dbd26
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-09-22 12:52:18 -0400

    recommendationsMenu init

---
+++ src/data/recommendationsMenu.js
@@ -0,0 +1,16 @@
+export const recommendationsMenu = {
+  id: "recommendations",
+  header: "Chef's Recommendations Menu",
+  paragraph: "",
+  items: [
+    {
+      description:
+        "white meat chicken sauteed with carrots, celery, and bean sprouts in Szechuan sauce",
+      header: "Szechuan Chicken",
+      label: "L12",
+      price: "$13.55",
+      img: "",
+      bg: "",
+    },
+  ],
+};
