9a3471de6c38c0e68e36cfc1fddccba0d0f515b5
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-09-22 10:16:17 -0400

    soupMenu init structure added

--- src/data/soupMenu.js
+++ src/data/soupMenu.js
@@ -3,5 +3,15 @@ export const soupMenu = {
   header: "Soup Menu",
   paragraph: "",
   header: "",
-  items: [],
+  items: [
+    {
+      description:
+        "white meat chicken sauteed with carrots, celery, and bean sprouts in Szechuan sauce",
+      header: "Szechuan Chicken",
+      label: "L12",
+      price: "$13.55",
+      img: "",
+      bg: "",
+    },
+  ],
 };
