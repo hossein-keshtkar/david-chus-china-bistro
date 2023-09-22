d5740c86d7019a307d3a170b7b6a8476b3d6b20c
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-09-22 10:46:46 -0400

    ProductsItem completed

--- src/components/menu/ProductsItem.jsx
+++ src/components/menu/ProductsItem.jsx
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import LazyImg from "../LazyImg";
+import LazyImage from "../LazyImage";
+import styles from "../../styles/ProductionItem.module.css";
 
 const ProductsItem = ({ description, header, label, price, img, bg }) => {
+  const [windowsWidth, setWindowsWidth] = useState(window.innerWidth);
+
+  const widthHandler = () => {
+    setWindowsWidth(window.innerWidth);
+    console.log(windowsWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", widthHandler);
+
+    return () => window.removeEventListener("resize", widthHandler);
+  });
+
   return (
-    <div className="d-flex flex-row">
-      <div>
-        <LazyImg src={img} blurred={bg} alt={label} />
-        <span>{label}</span>
-        <span>{price}</span>
+    <div
+      className={`${styles.container} d-flex flex-column flex-md-row align-items-center border-bottom py-4 py-md-0`}
+    >
+      <div className="position-relative">
+        <LazyImage
+          img={img}
+          bg={bg}
+          width={windowsWidth > 992 ? 177 : 250}
+          height={windowsWidth > 992 ? 106 : 150}
+        >
+          <span className="bg-success w-25 text-center position-absolute bottom-0 end-0">
+            {label}
+          </span>
+        </LazyImage>
+        <h5 className="mt-1">{price}</h5>
       </div>
-      <div className="ms-3">
-        <h2>{header}</h2>
+      <div
+        className={`${styles.content} mx-4 ms-md-4 text-center text-md-start`}
+      >
+        <h4>{header}</h4>
         <em>{description}</em>
       </div>
     </div>
