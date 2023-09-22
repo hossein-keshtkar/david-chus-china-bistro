ea323cb8b629fd525f78beaa7ea4211e5a81fc91
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-09-22 16:27:15 -0400

    soupMenu completed

--- src/data/soupMenu.js
+++ src/data/soupMenu.js
@@ -1,17 +1,126 @@
+import a1Bg from "../assets/images/menu/bgImg/soup/A1.png";
+import a1Img from "../assets/images/menu/mainImg/soup/A1.jpg";
+import a2Bg from "../assets/images/menu/bgImg/soup/A2.png";
+import a2Img from "../assets/images/menu/mainImg/soup/A2.jpg";
+import a3Bg from "../assets/images/menu/bgImg/soup/A3.png";
+import a3Img from "../assets/images/menu/mainImg/soup/A3.jpg";
+import a4Bg from "../assets/images/menu/bgImg/soup/A4.png";
+import a4Img from "../assets/images/menu/mainImg/soup/A4.jpg";
+import a5Bg from "../assets/images/menu/bgImg/soup/A5.png";
+import a5Img from "../assets/images/menu/mainImg/soup/A5.jpg";
+import a6Bg from "../assets/images/menu/bgImg/soup/A6.png";
+import a6Img from "../assets/images/menu/mainImg/soup/A6.jpg";
+import a7Bg from "../assets/images/menu/bgImg/soup/A7.png";
+import a7Img from "../assets/images/menu/mainImg/soup/A7.jpg";
+import a8Bg from "../assets/images/menu/bgImg/soup/A8.png";
+import a8Img from "../assets/images/menu/mainImg/soup/A8.jpg";
+import a9Bg from "../assets/images/menu/bgImg/soup/A9.png";
+import a9Img from "../assets/images/menu/mainImg/soup/A9.jpg";
+import a10Bg from "../assets/images/menu/bgImg/soup/A10.png";
+import a10Img from "../assets/images/menu/mainImg/soup/A10.jpg";
+import a11Bg from "../assets/images/menu/bgImg/soup/A11.png";
+import a11Img from "../assets/images/menu/mainImg/soup/A11.jpg";
+
 export const soupMenu = {
   id: "soup",
   header: "Soup Menu",
   paragraph: "",
-  header: "",
   items: [
     {
       description:
-        "white meat chicken sauteed with carrots, celery, and bean sprouts in Szechuan sauce",
-      header: "Szechuan Chicken",
-      label: "L12",
-      price: "$13.55",
-      img: "",
-      bg: "",
+        "chicken-stuffed won tons in clear chicken broth with white meat chicken pieces and a few scallions",
+      header: "Won Ton Soup with Chicken",
+      label: "A1",
+      price: "$5.50 pint $11.00 quart",
+      img: a1Img,
+      bg: a1Bg,
+    },
+    {
+      description: "chicken broth with egg drop",
+      header: "Egg Drop Soup",
+      label: "A2",
+      price: "$5.50 pint $11.00 quart",
+      img: a2Img,
+      bg: a2Bg,
+    },
+    {
+      description:
+        "clear chicken broth with creamy corn and egg drop with white meat chicken pieces",
+      header: "Chicken Corn Soup",
+      label: "A3",
+      price: "$5.50 pint $11.00 quart",
+      img: a3Img,
+      bg: a3Bg,
+    },
+    {
+      description: "tofu, chicken, mushroom, bamboo shoot, and egg",
+      header: "Hot and Sour Soup",
+      label: "A4",
+      price: "$5.50 pint $11.00 quart",
+      img: a4Img,
+      bg: a4Bg,
+    },
+    {
+      description: "chicken soup with egg drop and won tons",
+      header: "Egg Drop with Won Ton Soup",
+      label: "A5",
+      price: "$6.50 pint $13.00 quart",
+      img: a5Img,
+      bg: a5Bg,
+    },
+    {
+      description:
+        "clear broth and lo mein noodles or white rice, chicken pieces",
+      header: "Chicken Noodle (or Rice) Soup",
+      label: "A6",
+      price: "$5.50 pint $11.00 quart",
+      img: a6Img,
+      bg: a6Bg,
+    },
+    {
+      description:
+        "clear chicken broth with mixed vegetables (carrots, cabbage, baby corn, mushroom, snow peas)",
+      header: "Garden Vegetable Soup",
+      label: "A7",
+      price: "$6.00 pint $12.00 quart",
+      img: a7Img,
+      bg: a7Bg,
+    },
+    {
+      description:
+        "clear chicken broth with mixed vegetables (carrots, cabbage, baby corn, mushroom, snow peas) with tofu pieces",
+      header: "Garden Vegetable Soup with Tofu",
+      label: "A8",
+      price: "$6.50 pint $13.00 quart",
+      img: a8Img,
+      bg: a8Bg,
+    },
+    {
+      description:
+        "clear chicken broth with mixed vegetables (carrots, cabbage, baby corn, mushroom, snow peas) and chicken pieces",
+      header: "Chicken with Garden Vegetable Soup",
+      label: "A9",
+      price: "$6.50 pint $13.00 quart",
+      img: a9Img,
+      bg: a9Bg,
+    },
+    {
+      description:
+        "clear chicken broth with carrots, mushrooms, snow peas, and broccoli, and a few pieces of Hong Kong style won tons",
+      header: "Hong Kong Style Won Ton Soup",
+      label: "A10",
+      price: "$7.50 pint $15.00 quart",
+      img: a10Img,
+      bg: a10Bg,
+    },
+    {
+      description:
+        "clear chicken broth with vegetables, veal, chicken, and beef and won tons",
+      header: "Young Chow Won Ton Soup (for 2)",
+      label: "A11",
+      price: "$19.95",
+      img: a11Img,
+      bg: a11Bg,
     },
   ],
 };
