95489cb3afe5a56f5cd5d9f2dc9621331bb34680
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-09-22 17:09:11 -0400

    h1 used instead of h3

--- src/components/menu/MenuPage.jsx
+++ src/components/menu/MenuPage.jsx
@@ -9,7 +9,7 @@ const Menu = () => {
   return (
     <Page>
       <Container className={styles.Container}>
-        <h3 className="text-center mt-5">Menu Categories</h3>
+        <h1 className="text-center mt-5">Menu Categories</h1>
         <p className="text-center">
           Substituting white rice with brown rice or fried rice after 3:00pm
           will be $1.50 for a pint and $2.50 for a quart.
