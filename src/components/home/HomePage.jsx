b25e5e1860d56cb67ee3bdffef2f81ea445e619c
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-09-22 13:58:57 -0400

    using LazyImage instead of LazyImg

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -5,20 +5,21 @@ import HomeRow from "./HomeRow";
 import imgLg from "../../assets/images/jumbotron_1200.jpg";
 import imgMd from "../../assets/images/jumbotron_992.jpg";
 import imgSm from "../../assets/images/jumbotron_768.jpg";
-import blurred from "../../assets/images/blury-resturant.png";
-import LazyImg from "../LazyImg";
+import bg from "../../assets/images/blury-resturant.png";
 import styles from "../../styles/HomePage.module.css";
 import Page from "../Page";
+import LazyImage from "../LazyImage";
 
 const Home = () => {
   return (
     <Page>
       <Container className={`${styles.Container} mt-3`}>
-        <LazyImg
-          blurred={blurred}
+        <LazyImage
+          bg={bg}
           src={imgSm}
           srcSet={`${imgSm} 768w, ${imgMd} 992w, ${imgLg} 1440w`}
           sizes="(max-width: 768px) 100vw, (max-width: 992px) 100vw, 90vw"
+          style={{ aspectRatio: "2/1.2" }}
         />
         <HomeRow />
       </Container>
