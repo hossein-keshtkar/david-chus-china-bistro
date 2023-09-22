a0cd897eac7bd83c3894a0bd10a8270f11353fa9
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-09-22 17:43:10 -0400

    appetizerMenu completed

---
+++ src/data/appetizersMenu.js
@@ -0,0 +1,174 @@
+import b1Bg from "../assets/images/menu/bgImg/appetizer/B1.png";
+import b1Img from "../assets/images/menu/mainImg/appetizer/B1.jpg";
+import b2Bg from "../assets/images/menu/bgImg/appetizer/B2.png";
+import b2Img from "../assets/images/menu/mainImg/appetizer/B2.jpg";
+import b3Bg from "../assets/images/menu/bgImg/appetizer/B3.png";
+import b3Img from "../assets/images/menu/mainImg/appetizer/B3.jpg";
+import b4Bg from "../assets/images/menu/bgImg/appetizer/B4.png";
+import b4Img from "../assets/images/menu/mainImg/appetizer/B4.jpg";
+import b5Bg from "../assets/images/menu/bgImg/appetizer/B5.png";
+import b5Img from "../assets/images/menu/mainImg/appetizer/B5.jpg";
+import b6Bg from "../assets/images/menu/bgImg/appetizer/B6.png";
+import b6Img from "../assets/images/menu/mainImg/appetizer/B6.jpg";
+import b7Bg from "../assets/images/menu/bgImg/appetizer/B7.png";
+import b7Img from "../assets/images/menu/mainImg/appetizer/B7.jpg";
+import b8Bg from "../assets/images/menu/bgImg/appetizer/B8.png";
+import b8Img from "../assets/images/menu/mainImg/appetizer/B8.jpg";
+import b9Bg from "../assets/images/menu/bgImg/appetizer/B9.png";
+import b9Img from "../assets/images/menu/mainImg/appetizer/B9.jpg";
+import b10Bg from "../assets/images/menu/bgImg/appetizer/B10.png";
+import b10Img from "../assets/images/menu/mainImg/appetizer/B10.jpg";
+import b11Bg from "../assets/images/menu/bgImg/appetizer/B11.png";
+import b11Img from "../assets/images/menu/mainImg/appetizer/B11.jpg";
+import b12Bg from "../assets/images/menu/bgImg/appetizer/B12.png";
+import b12Img from "../assets/images/menu/mainImg/appetizer/B12.jpg";
+import b13Bg from "../assets/images/menu/bgImg/appetizer/B13.png";
+import b13Img from "../assets/images/menu/mainImg/appetizer/B13.jpg";
+import b14Bg from "../assets/images/menu/bgImg/appetizer/B14.png";
+import b14Img from "../assets/images/menu/mainImg/appetizer/B14.jpg";
+import b15Bg from "../assets/images/menu/bgImg/appetizer/B15.png";
+import b15Img from "../assets/images/menu/mainImg/appetizer/B15.jpg";
+import b16Bg from "../assets/images/menu/bgImg/appetizer/B16.png";
+import b16Img from "../assets/images/menu/mainImg/appetizer/B16.jpg";
+
+export const appetizersMenu = {
+  id: "appetizer",
+  header: "Appetizers Menu",
+  paragraph: "",
+  items: [
+    {
+      description: "eggroll with cabbage, carrots and beef",
+      header: "Beef Egg Roll",
+      label: "B1",
+      price: "$3.95",
+      img: b1Img,
+      bg: b1Bg,
+    },
+    {
+      description: "thin wraps with white meat and cabbage",
+      header: "Spring Roll (1)",
+      label: "B2",
+      price: "$3.95",
+      img: b2Img,
+      bg: b2Bg,
+    },
+    {
+      description: "cabbage and carrots in eggroll wrappers",
+      header: "Vegetable Egg Roll (1)",
+      label: "B3",
+      price: "$3.50",
+      img: b3Img,
+      bg: b3Bg,
+    },
+    {
+      description:
+        "triangle shaped won ton with ground white meat chicken inside",
+      header: "Fried Won Ton with Chicken Meat (6)",
+      label: "B4",
+      price: "$12.95",
+      img: b4Img,
+      bg: b4Bg,
+    },
+    {
+      description:
+        "ground chicken meat on bread, deep-fried, comes with 4 pieces",
+      header: "Chicken Toast (4)",
+      label: "B5",
+      price: "$12.95",
+      img: b5Img,
+      bg: b5Bg,
+    },
+    {
+      description:
+        "4 large tofu cubes, breaded and deep-fried, with garlic sauce on the side",
+      header: "Fried Silky Tofu with Special Garlic Sauce",
+      label: "B6",
+      price: "$12.95",
+      img: b6Img,
+      bg: b6Bg,
+    },
+    {
+      description: "dough mixed with scallion and pan-fried",
+      header: "Scallion Pancake",
+      label: "B7",
+      price: "$12.95",
+      img: b7Img,
+      bg: b7Bg,
+    },
+    {
+      description: "house-made dough dumpling with chicken",
+      header: "Steamed (or Pan Fried) Chicken Dumplings (6)",
+      label: "B8",
+      price: "$13.95",
+      img: b8Img,
+      bg: b8Bg,
+    },
+    {
+      description:
+        "house-made dough dumpling with carrot, mushroom, cellophane noodles, cabbage (6 pieces)",
+      header: "Steamed (or Pan Fried) Vegetable Dumplings (6)",
+      label: "B9",
+      price: "$13.95",
+      img: b9Img,
+      bg: b9Bg,
+    },
+    {
+      description: "soft won tons filled with chicken, with garlic sauce",
+      header: "Szechuan Soft Won Ton (8)",
+      label: "B10",
+      price: "$13.95",
+      img: b10Img,
+      bg: b10Bg,
+    },
+    {
+      description:
+        "white-meat chicken with mushrooms, green peppers, water chestnuts, carrots sauteed with special house sauce, wrapped in fresh lettuce",
+      header: "Chicken in Soothing Lettuce Wraps",
+      label: "B11",
+      price: "$14.95",
+      img: b11Img,
+      bg: b11Bg,
+    },
+    {
+      description: "6 pieces of beef on skewers with teriyaki sauce",
+      header: "Teriyaki Beef (6)",
+      label: "B12",
+      price: "$13.95",
+      img: b12Img,
+      bg: b12Bg,
+    },
+    {
+      description: "6 pieces of curry-flavored chicken wings",
+      header: "Fried Chicken Wing (6)",
+      label: "B13",
+      price: "$9.95",
+      img: b13Img,
+      bg: b13Bg,
+    },
+    {
+      description: "marinated grilled roast barbeque ribs",
+      header: "B.B.Q. Spareribs",
+      label: "B14",
+      price: "$17.95",
+      img: b14Img,
+      bg: b14Bg,
+    },
+    {
+      description:
+        "2 spring egg rolls, 2 fried won tons, 2 BBQ ribs, 2 chicken toast, 2 teriyaki beef",
+      header: "Pu Pu Platter (for 2)",
+      label: "B15",
+      price: "$23.95",
+      img: b15Img,
+      bg: b15Bg,
+    },
+    {
+      description: "Peanut butter sauce and sesame seeds on lo mein noodles",
+      header: "Cold Sesame Noodle",
+      label: "B16",
+      price: "$12.95",
+      img: b16Img,
+      bg: b16Bg,
+    },
+  ],
+};
--- src/data/menuSubCategories.js
+++ src/data/menuSubCategories.js
@@ -1,5 +1,11 @@
+import { appetizersMenu } from "./appetizersMenu";
 import { lunchMenu } from "./lunchMenu";
 import { recommendationsMenu } from "./recommendationsMenu";
 import { soupMenu } from "./soupMenu";
 
-export const menuSubCategories = [lunchMenu, soupMenu, recommendationsMenu];
+export const menuSubCategories = [
+  lunchMenu,
+  soupMenu,
+  recommendationsMenu,
+  appetizersMenu,
+];
