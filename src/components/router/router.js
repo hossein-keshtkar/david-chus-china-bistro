dcd58ab9ebb6d3c9988486d6a77842109ee644a9
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-09-22 13:13:15 -0400

    Specials repleaced with recommendationsMenu route

--- src/components/router/router.js
+++ src/components/router/router.js
@@ -4,14 +4,12 @@ import { lazy } from "react";
 const Home = lazy(() => import("../home/HomePage"));
 const Login = lazy(() => import("../login/LoginPage"));
 const Menu = lazy(() => import("../menu/MenuPage"));
-const Specials = lazy(() => import("../specials/SpecialsPage"));
 const Products = lazy(() => import("../menu/Products"));
 const NotFound = lazy(() => import("../NotFound"));
 
 const homeApi = process.env.REACT_APP_HOME_API;
 const loginApi = process.env.REACT_APP_ADMIN_API;
 const menuApi = process.env.REACT_APP_MENU_API;
-const specialsApi = process.env.REACT_APP_SPECIALS_API;
 const productsApi = process.env.REACT_APP_PRODUCT_API;
 
 export const router = createBrowserRouter([
@@ -26,10 +24,6 @@ export const router = createBrowserRouter([
   {
     path: menuApi,
     element: <Menu />,
-  },
-  {
-    path: specialsApi,
-    element: <Specials />,
   },
   {
     path: productsApi,
