4c7744ce2af13ecbfba8c277af433160e9bb53ca
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-09-22 13:08:50 -0400

    recommendationsMenu completed

--- src/data/recommendationsMenu.js
+++ src/data/recommendationsMenu.js
@@ -1,3 +1,20 @@
+import sp1Bg from "../assets/images/menu/bgImg/SP/SP1.png";
+import sp1Img from "../assets/images/menu/mainImg/SP/SP1.jpg";
+import sp2Bg from "../assets/images/menu/bgImg/SP/SP2.png";
+import sp2Img from "../assets/images/menu/mainImg/SP/SP2.jpg";
+import sp3Bg from "../assets/images/menu/bgImg/SP/SP3.png";
+import sp3Img from "../assets/images/menu/mainImg/SP/SP3.jpg";
+import sp4Bg from "../assets/images/menu/bgImg/SP/SP4.png";
+import sp4Img from "../assets/images/menu/mainImg/SP/SP4.jpg";
+import sp5Bg from "../assets/images/menu/bgImg/SP/SP5.png";
+import sp5Img from "../assets/images/menu/mainImg/SP/SP5.jpg";
+import sp6Bg from "../assets/images/menu/bgImg/SP/SP6.png";
+import sp6Img from "../assets/images/menu/mainImg/SP/SP6.jpg";
+import sp7Bg from "../assets/images/menu/bgImg/SP/SP7.png";
+import sp7Img from "../assets/images/menu/mainImg/SP/SP7.jpg";
+import sp8Bg from "../assets/images/menu/bgImg/SP/SP8.png";
+import sp8Img from "../assets/images/menu/mainImg/SP/SP8.jpg";
+
 export const recommendationsMenu = {
   id: "recommendations",
   header: "Chef's Recommendations Menu",
@@ -5,12 +22,75 @@ export const recommendationsMenu = {
   items: [
     {
       description:
-        "white meat chicken sauteed with carrots, celery, and bean sprouts in Szechuan sauce",
-      header: "Szechuan Chicken",
-      label: "L12",
-      price: "$13.55",
-      img: "",
-      bg: "",
+        "with choice of string bean, string bean chicken, string bean beef, beef onions, moo shu vegetable",
+      header: "Chinese Scallion Pancake Wrap",
+      label: "SP1",
+      price: "$26.95",
+      img: sp1Img,
+      bg: sp1Bg,
+    },
+    {
+      description:
+        "marinated grilled chicken breast with vegetables and lo mein on the side",
+      header: "Teriyaki Chicken",
+      label: "SP2",
+      price: "$23.95",
+      img: sp2Img,
+      bg: sp2Bg,
+    },
+    {
+      description:
+        "assorted vegetables breaded and fried, served with lo mein on the side",
+      header: "Vegetable Tempura",
+      label: "SP3",
+      price: "$24.95",
+      img: sp3Img,
+      bg: sp3Bg,
+    },
+    {
+      description:
+        "white meat chicken and beef, breaded and deep-fried with special house sauce, served with lo mein and vegetables",
+      header: "Orange Chicken and Beef Combo",
+      label: "SP4",
+      price: "$25.95",
+      img: sp4Img,
+      bg: sp4Bg,
+    },
+    {
+      description:
+        "beef, veal, and chicken sauteed with mixed vegetables in chef's special sauce, lo mein served on the side",
+      header: "Happy Family",
+      label: "SP5",
+      price: "$25.95",
+      img: sp5Img,
+      bg: sp5Bg,
+    },
+    {
+      description:
+        "marinated and grilled veal served with vegetables and lo mein on the side",
+      header: "Grilled Veal",
+      label: "SP6",
+      price: "$24.95",
+      img: sp6Img,
+      bg: sp6Bg,
+    },
+    {
+      description:
+        "a few cuts of beef, breaded, deep-fried with sauce containing orange peels, served with lo mein on the side",
+      header: "Orange Beef",
+      label: "SP7",
+      price: "$24.95",
+      img: sp7Img,
+      bg: sp7Bg,
+    },
+    {
+      description:
+        "a few cuts of beef, breaded, deep-fried with sauce containing sesame seeds, served with lo mein on the side",
+      header: "Sesame Beef",
+      label: "SP8",
+      price: "$24.95",
+      img: sp8Img,
+      bg: sp8Bg,
     },
   ],
 };
