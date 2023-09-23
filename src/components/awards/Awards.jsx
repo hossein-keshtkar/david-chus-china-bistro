0a05e1f1bbee82be76fa106f2df0da5f4eeea13a
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-09-23 19:05:54 -0400

    Awards updated

--- src/components/awards/Awards.jsx
+++ src/components/awards/Awards.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 
 import Page from "../Page";
+import { Container } from "react-bootstrap";
 
 const Awards = () => {
-  return <Page>Awards</Page>;
+  return (
+    <Page>
+      <Container className="text-center">
+        <h1>Awards</h1>
+        <p>
+          David Chu's China Bistro is a recepient of multiple awards for its
+          great food and service.
+          <br />
+          Details are coming soon.
+        </p>
+      </Container>
+    </Page>
+  );
 };
 
 export default Awards;
