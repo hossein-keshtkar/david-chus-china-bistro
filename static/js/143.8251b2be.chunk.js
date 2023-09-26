15bae25ecf29796c24a9d0eb398f52cfaddbc718
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-09-26 10:49:49 -0400

    Updates

---
+++ static/js/143.8251b2be.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdavid_chus_china_bistro=self.webpackChunkdavid_chus_china_bistro||[]).push([[143],{8293:function(s,e,i){i.r(e);i(2791);var r=i(412),a=i(7022),n=i(184);e.default=function(){return(0,n.jsx)(r.Z,{children:(0,n.jsxs)(a.Z,{className:"text-center my-5",children:[(0,n.jsx)("h1",{children:"Awards"}),(0,n.jsxs)("p",{children:["David Chu's China Bistro is a recepient of multiple awards for its great food and service.",(0,n.jsx)("br",{}),"Details are coming soon."]})]})})}}}]);
+//# sourceMappingURL=143.8251b2be.chunk.js.map
